1119db2a03da886b15f8b6a2af0bbc0bb43342d0
CodeGuruKrish <hithesh.hithesh@gmail.com>
2016-11-20 22:11:51 -0600

    Removed the testing comment

--- testfile.java
+++ testfile.java
@@ -3,6 +3,5 @@ Class Test{
 public static void main(String args[])
 {
 	System.out.println("Hello World i am hithesh here");
-	System.out.println("Just testing this link https://github.com/CodeGuruKrish/TestProject.git");
 }
 }
